f2551483eb01e1a321debb0fd679f5c66a68a25f
Giulietta De Santis <82796947+Oanhh37030@users.noreply.github.com>
2021-08-25 19:47:56 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,8 @@
 s (3 sloc) 60 Bytes
 @echo off
-echo You can login now.
-ping -n 10 127.0.0.1 >nul
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
+echo User: Administrator
+echo Pass: Thuonghai001
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
+ping -n 10 127.0.0.1 >nul 
